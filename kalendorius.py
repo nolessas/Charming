13a9cb4247944c8f1177bd52e067d0063123fc8e
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-15 00:20:31 +0200

    Update kalendorius.py

--- kalendorius.py
+++ kalendorius.py
@@ -23,7 +23,7 @@ def fetch_client_data_for_calendar():
     events = []
     for _, row in df.iterrows():
         event = {
-            'start': row['Date'].strftime('%H:%M')
+            'start': row['Date'].isoformat(),
             'end': (row['Date'] + pd.DateOffset(hours=1)).isoformat(),
             'color': 'blue'  # or any other color
         }
