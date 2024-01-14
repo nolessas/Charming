9ea63bbcb1720ce6dd21d540151597d9c54f6b21
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-14 16:12:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -305,34 +305,11 @@ def show_dashboard():
     elif choose_main == "option3":
         # Option 3: Add Item to Sheet2 and Display Data
         st.title("Data from Sheet3")
-        st.write("Reikalingos priemones ir kur jas rasti.")
-
+        st.write("Reikalingos priemones")
         item_input = st.text_input("Reikalingos priemones:", key="item")
         location_input = st.text_input("Kur:", key="location")
         if st.button("Add Entry", key="add"):
             add_item_to_sheet2(item_input, location_input)
-
-        # Fetch and display data from Google Sheets
-        records = fetch_data_from_sheets()
-        if records:
-            df = pd.DataFrame(records)
-            # Add a selectbox for sorting options
-            sort_option = st.selectbox("Sort by:", df.columns, index=1)
-            sort_ascending = st.checkbox("Ascending Order", value=True)
-            df = df.sort_values(by=[sort_option], ascending=sort_ascending)
-
-            # Display the DataFrame with a delete button for each row
-            for index, row in df.iterrows():
-                cols = st.columns([2, 1, 1])
-                with cols[0]:  # Display the data
-                    st.text(f"{row['Reikalingos priemones']} - {row['Kur']}")
-                with cols[1]:  # Display the delete button
-                    if st.button('Delete', key=f"delete_{index}"):
-                        # Delete the selected row from the DataFrame
-                        df = df.drop(index)
-                        st.success("Row deleted successfully!")
-            
-            st.dataframe(df)
 
 
 
