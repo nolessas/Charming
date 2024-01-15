9442fcd63af28a565a33fb6a020cc5ca224e72f7
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-15 13:12:59 +0000

    commit

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import streamlit as st
 from authentication import is_user_logged_in, show_login, set_user_logged_in
 from datetime import datetime
+from datetime import time
 import gspread
 from google.oauth2 import service_account
 import pandas as pd
