db38266b3be1ad6e0407e6e48d1c1a69996d2b0e
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-16 10:54:34 +0000

    commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import time, datetime
 import gspread
 from google.oauth2 import service_account
 import pandas as pd
-from kalendorius import display_calendar
+
 import streamlit as st
 from pathlib import Path
 from streamlit_calendar import calendar
