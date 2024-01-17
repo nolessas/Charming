824cb8b06a41bdfcce8fb44507a23fbb64b8986d
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-17 00:34:19 +0000

    commit

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def show_dashboard():
 
     elif choose_main == "4":
         st.title("DoDo")
-        #register_todo()
-        manage_todo_list()
+        register_todo()
+        #manage_todo_list()
 
     elif choose_main == "5":
         st.write("This tab is on development")
