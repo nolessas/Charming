436cf0f4b753da183e86d66f394ce37f2e0a7103
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-18 18:58:54 +0000

    Remove unused image display code

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def main():
 
 
 
-st.image("/image.png")
+
 def show_dashboard():
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
     st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
