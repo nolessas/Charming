e5c95ee94db174cf3a6d5c22a500ec3850774f24
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-18 12:43:45 +0000

    Refactor To_do.py and app.py

--- To_do.py
+++ To_do.py
@@ -7,14 +7,34 @@ from google_sheets import get_sheets_service
 
 def register_todo():
     st.title("")
-    item = st.text_input("Enter item")
-    location = st.text_input("Enter location")
-    if st.button("Register"):
-        add_item_to_sheet2(item, location)
-        st.experimental_rerun()  # Rerun the app to refresh the data display
 
+    # Fetch data from Google Sheets
+    records = fetch_data_from_sheets2()
 
+    if not records:
+        st.write("No clients found.")
+        return
+
+    # Convert records to DataFrame
+    df = pd.DataFrame(records)
+
+    # Initialize an empty list to store indices of selected rows
+    selected_indices = []
+
+    # Display each client with a checkbox
+    for index, row in df.iterrows():
+        if st.checkbox(f"{row['Keik']}{row['Ko']}", key=index):
+            selected_indices.append(index)
+
+    # Confirm deletion button
+    if st.button('Confirm deletion of selected clients'):
+        for i in selected_indices:
+            delete_row_from_sheet2(i, records)  # Delete selected clients
+        st.success("Selected clients deleted successfully!")
+        st.rerun()  # Rerun the app to refresh the data display
     
+
+
 def add_item_to_sheet2(item, location):
     service = get_sheets_service()
     spreadsheet_id = '1HR8NzxkcKKVaWCPTowXdYtDN5dVqkbBeXFsHW4nmWCQ'
