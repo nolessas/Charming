5ccdb363ebe923726798ca24f411f3c75cdbf901
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-19 12:25:39 +0000

    Add button to open Google Sheets

--- app.py
+++ app.py
@@ -59,7 +59,13 @@ def show_dashboard():
         # Using markdown to open link in a new tab
         if st.button('Open Google Calendar'):
             st.markdown(f'<a href="{google_calendar_url}" target="_blank">Click here to open Google Calendar</a>', unsafe_allow_html=True)
+        google_sheets_url = "https://docs.google.com/spreadsheets/d/1HR8NzxkcKKVaWCPTowXdYtDN5dVqkbBeXFsHW4nmWCQ/edit#gid=0"
 
+        # Creating a button to open Google Sheets
+        if st.button('Open Google Sheets'):
+            js = f"window.open('{google_sheets_url}')"  # JavaScript to open a new window/tab
+            html = f'<img src onerror="{js}">'  # HTML to execute the JavaScript
+            st.markdown(html, unsafe_allow_html=True)
 
     elif choose_main == "3":
         st.title("Pašalinti klientą")
