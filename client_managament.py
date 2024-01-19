f3893a2ccdc9fae87a3757f4cc3ec985c458f481
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-19 17:00:13 +0000

    Update phone number column name in client management module

--- client_managament.py
+++ client_managament.py
@@ -65,8 +65,8 @@ def show_registered_clients():
             
             df['Weekday'] = df['Date'].dt.day_name().map(day_name_map)
             
-            if 'Phone Number' in df.columns:
-                df['Phone Number'] = df['Phone Number'].astype(str)
+            if 'Phone' in df.columns:
+                df['Phone'] = df['Phone'].astype(str)
 
             # Format the 'Date' column to display without the time part
             df['Date'] = df['Date'].dt.strftime('%Y-%m-%d')
