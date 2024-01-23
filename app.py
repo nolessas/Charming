4b406c20a6bfecc8c869a705ff23837bb2f443b0
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-23 23:40:18 +0000

    Remove archive_old_clients function

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from streamlit_calendar import calendar
 from kalendorius import display_calendar
 import streamlit as st
 from pathlib import Path
-from client_managament import show_registered_clients, register_client1, get_sheets_service, write_to_sheets, delete_client, edit_appointment_details, archive_old_clients
+from client_managament import show_registered_clients, register_client1, get_sheets_service, write_to_sheets, delete_client, edit_appointment_details
 from data_base import show_clients_with_date_filter, delete_row_from_sheet, fetch_data_from_sheets
 from To_do import register_todo, add_item_to_sheet2, delete_row_from_sheet2, fetch_data_from_sheets2
 
