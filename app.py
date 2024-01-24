3e815a332f687e4c33a049fe7daea99757eed5f0
nolessas <155294980+nolessas@users.noreply.github.com>
2024-01-24 00:23:53 +0000

    Update client appointment title

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def show_dashboard():
       
 
     elif choose_main == "6":
-        st.title("Keisti kliento atvykimo dieną")
+        st.title("Keisti kliento duomenis arba užregistruoti dar karta")
         client_name_to_edit = str(st.text_input("Iveskite kliento Vardą Pavardę:"))
         if client_name_to_edit:
             edit_appointment_details(client_name_to_edit)
